e624f8b69c576a3efed07237dc183b423ff32f04
Khattiya <khattiya.dokbua@gmail.com>
2023-12-19 00:36:11 +0700

    22

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import express from "express";
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 const corsOptions = {
   origin: "*",
